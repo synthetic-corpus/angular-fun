f36336ff9c4459a544ddead611d1535b221330ce
Joel Gonzaga <jgonzaga@USAePAYs-MacBook-Pro-2.local>
2017-04-25 17:28:31 -0700

    added the outline of the game assignment.

--- assingment-2/src/app/app.module.ts
+++ assingment-2/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ControlComponent } from './control/control.component';
+import { OddComponent } from './control/odd/odd.component';
+import { EvenComponent } from './control/even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
