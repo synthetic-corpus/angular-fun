3020185cb8679534b4a41bde5840ea928370b927
Joel Gonzaga <jgonzaga@USAePAYs-MacBook-Pro-2.local>
2017-04-26 11:32:47 -0700

    everything is broken and nothing works.

--- assingment-2/src/app/control/control.component.ts
+++ assingment-2/src/app/control/control.component.ts
@@ -1,32 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, DoCheck, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   templateUrl: './control.component.html',
   styleUrls: ['./control.component.css']
 })
-export class ControlComponent implements OnInit {
-  gameRunning = false;
-  someNumber = 0;
+export class ControlComponent implements OnInit{
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber =0;
+  constructor(){}
+  ngOnInit(){
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
-  startGame() {
-    this.gameRunning = true;
-  }
-
-  pauseGame() {
-    this.gameRunning = false;
-  }
-
-  emitNumber() {
-    while (this.gameRunning) {
-      //send numbers
-    }
-
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber+1);
+    },1000);
   }
 
 }
