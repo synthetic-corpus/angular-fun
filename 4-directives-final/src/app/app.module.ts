7edd957dd98858e954f095e9ac694cd300b71f37
Syn <joel.gonzaga@gmail.com>
2017-05-03 16:49:20 -0700

    adding the services start folder.

--- 4-directives-final/src/app/app.module.ts
+++ 4-directives-final/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+// Custom directive must be imported here. As with other modules, .ts is removed.
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 import { UnlessDirective } from './unless.directive';
 
 @NgModule({
+  // Custom directives must also be written here.
   declarations: [
     AppComponent,
     BasicHighlightDirective,
