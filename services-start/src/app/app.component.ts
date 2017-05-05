98528b531905cda01414248f5958272872d5987b
Syn <joel.gonzaga@gmail.com>
2017-05-04 19:44:49 -0700

    added the Accounts service.

--- services-start/src/app/app.component.ts
+++ services-start/src/app/app.component.ts
@@ -1,31 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[AccountsService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
+  // Intiate variable acounts. Contains objects with two strings. Is empty at first.
+  accounts: {name: string, status: string}[] =[];
 
+  // inject accounts service.
+  constructor(private accountsZ: AccountsService) {
+
+  }
+
+  OnInit(){
+    this.accounts = this.accountsZ.accounts;
+  }
+
+
+  /* This code move to a service along with the array.
   onAccountAdded(newAccount: {name: string, status: string}) {
     this.accounts.push(newAccount);
   }
 
   onStatusChanged(updateInfo: {id: number, newStatus: string}) {
     this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  }
+  } */
 }
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,26 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service'
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers:[LoggingService]// This line needed to inject the dependecies.
+  providers:[LoggingService, AccountsService]// This line needed to inject the dependecies.
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   // This constructor instiates a class of the Loggin.service.ts
   // Not sure why 'private is needed'
   // 'loggingService' is an arbitrary name. Other side of colon is not.
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService,
+              private accountsZ: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountsZ.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
     // console.log('A server status changed, new status: ' + accountStatus);
   }
