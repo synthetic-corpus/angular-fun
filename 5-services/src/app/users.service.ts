5ef41e774447012c956423c807dc44b98834c768
Syn <joel.gonzaga@gmail.com>
2017-05-07 15:06:06 -0700

    set the add and remove methods in the users.service

--- 5-services/src/app/users.service.ts
+++ 5-services/src/app/users.service.ts
@@ -1,4 +1,22 @@
 export class UsersService {
-  activeUser = [];
+  activeUsers = [];
   inactiveUsers = [];
+
+  setActive(string){
+    this.activeUsers.push(string);
+    if (string in this.inactiveUsers && this.inactiveUsers.length > 0) {
+      var index = this.inactiveUsers.indexOf(string);
+      this.inactiveUsers.splice(index, 1);
+
+    }
+  }
+
+  setInactive(string){
+    this.inactiveUsers.push(string);
+    if (string in this.activeUsers && this.activeUsers.length > 0) {
+      var index = this.activeUsers.indexOf(string);
+      this.activeUsers.splice(index, 1);
+
+    }
+  }
 }
