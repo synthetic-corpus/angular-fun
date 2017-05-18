7d331ce68c42999457c832569dcff2ad42cda4e7
Syn <joel.gonzaga@gmail.com>
2017-05-18 10:12:21 -0700

    added fragments and queryr params.

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -12,12 +12,14 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
-  onLoadServers(){
+  onLoadServers(id:number){
     // This is an example of loading a route via programs.
     // "I did a complex thing. Now navigate somewhere!"
     // Navigator methods takes arguements as array.
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id,'edit'], {queryParams:{allowEdit:'1'},
+    fragment:'loading'});
     // console.log("did a thing");
+    // Router Link is localhost:4200/servers/:id/edit/?allowEdit=1#loading 
   }
 
 }
