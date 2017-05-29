4b19613cb01bc6dd007f93be3cf4a0f312680241
Syn <joel.gonzaga@gmail.com>
2017-05-29 12:19:09 -0700

    Yay! I learned about forms todaygit add --all

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+  @ViewChild('f') signupForm: NgForm;
+
+  onSubmit(form: NgForm){
+      console.log(this.signupForm);
+  }
+  /* Old method. Returns and HTML object.
+  onSubmit(form: HTMLFormElement) {
+    console.log(form);
+  }*/
+  /* Not Viewchild method.
+   Why both with ViewChild? I do not know.
+  onSubmit(form: NgForm){
+    console.log(form);
+  }*/
+}
