9c79f7c16b8ba8212111ba45ca7431d971259c4d
Syn <joel.gonzaga@gmail.com>
2017-06-05 19:35:39 -0700

    Have the first entries of the reactive form written.

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders = ['male', 'female', 'trans', 'fluid','alien'];
+  // FormGroup is the first important import for reactive forms.
+  // FormGroups may be nested within FormGroups.
+  signupFormX: FormGroup;
+
+  ngOnInit() {
+    // Initialize the form here. This lifecycle hook runs before rendering.
+
+    /*
+    Form Group takes a Javascript {} object as its argument.
+    */
+    this.signupFormX = new FormGroup({
+      // First argument of FormControl is the initial value.
+      // Rest are option. Validators.
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gener': new FormControl('male')
+    });
+  }
 }
--- forms-reactive-start/src/app/app.module.ts
+++ forms-reactive-start/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+/* Not required for reactive templates.
+import { FormsModule } from '@angular/forms'; */
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -11,7 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
