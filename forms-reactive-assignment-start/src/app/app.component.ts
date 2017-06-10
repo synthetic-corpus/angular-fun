78e0f2384adf0ddca1f3b28519fbefb9ada0fec5
Syn <joel.gonzaga@gmail.com>
2017-06-10 15:04:05 -0700

    Completed the reactive form control thing.

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -8,14 +8,28 @@ import {FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   projectFormX: FormGroup;
+  badNames =['test'];
 
   ngOnInit(){
     this.projectFormX = new FormGroup(
       {
-        'projectNomen': new FormControl(null,[Validators.required]),
+        'projectNomen': new FormControl(null,[Validators.required,this.checkBadNames.bind(this)]),
         'email': new FormControl(null,[Validators.required, Validators.email]),
         'status': new FormControl(null,[Validators.required])
       }
     )
+  }
+
+  onSubmit(){
+    console.log(this.projectFormX);
+  }
+
+  checkBadNames(nomen: FormControl){
+    if (this.badNames.indexOf(nomen.value) != -1){
+      // Return the error message
+      return {'badName': true}
+    }else{
+      return null;
+    }
   }
 }
