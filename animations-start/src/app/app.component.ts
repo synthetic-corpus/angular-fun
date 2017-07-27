dd0c74e77c2d6b4b21bea8900d78a7117a06a0d9
Syn <joel.gonzaga@gmail.com>
2017-07-26 17:52:51 -0700

    created first animation. Corrected bug that made animated square flash unexpectedly.

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,15 +1,51 @@
 import { Component } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
+/* What do the import do?
+  Trigger is entered in the animations array. Holds the name of the the state and Transition
+  State holds the names of states, and style as paramaters.
+  Style contians a CSS styling change.
+  Transition controls the movement between them.
+*/
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  /* Animations Declared Here */
+  animations: [
+    /* Animations Triggers are declared with arbitrary names. In first Param
+      An Array of animation metadata */
+    trigger('divstate', [
+      // An array of functions.
+      // Paramaters are:
+      // (Name of state,style({ CSS Transitions })
+      state('normal', style({
+        'background-color':'green',
+        transform:'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform:'translateX(100px)'
+      })),
+      transition('normal => highlighted', animate(1000)),
+      transition('highlighted => normal',animate(400))
+    ])
+  ]
 })
 export class AppComponent {
 	list = ['Milk', 'Sugar', 'Bread'];
-
+  stateZ = 'normal';
 	onAdd(item) {
 		this.list.push(item);
 	}
+
+  onAnimate(){
+    this.stateZ == 'normal' ? this.stateZ = 'highlighted' : this.stateZ = 'normal';
+  }
 
 	onDelete(item) {
 		this.list.splice(this.list.indexOf(item), 1);
